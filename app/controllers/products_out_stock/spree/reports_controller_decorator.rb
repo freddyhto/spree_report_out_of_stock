640f6fe39cb2b219498aaae5f298929b2d58eb11
Freddy Tellez <ftellez@acid.cl>
2020-08-10 15:53:51 -0400

    fix filter by deleted_at

--- app/controllers/products_out_stock/spree/reports_controller_decorator.rb
+++ app/controllers/products_out_stock/spree/reports_controller_decorator.rb
@@ -14,25 +14,25 @@ module ProductsOutStock
       def products_out_stock
         @inventorys = []
         @search = ::Spree::StockMovement.includes(stock_item: [:stock_location, variant: [:product]] ).ransack(params[:q])
+        ::Spree::StockItem.only_deleted.where(deleted_filter_query, {
+          deleted_at_gteq: params[:q][:created_at_gteq], deleted_at_lt: params[:q][:created_at_lt]
+        }).each do |deleted_stock_item|
+          @inventorys << {
+            date: deleted_stock_item.deleted_at,
+            product: deleted_stock_item.variant.product,
+            location: deleted_stock_item.stock_location
+          }
+        end
         @search.result.group_by(&:stock_item_id).each do |item_id, movements|
           total = 0
-          delete_stock_item = ::Spree::StockItem.only_deleted.ransack(params[:q]).result.find_by id: item_id
-          if delete_stock_item
-            @inventorys << {
-              date: delete_stock_item.deleted_at,
-              product: delete_stock_item.variant.product,
-              location: delete_stock_item.stock_location
-            }
-          else
-            movements.each do |movement|
-              total += movement.quantity
-              if total == 0
-                @inventorys << {
-                  date: movement.created_at,
-                  product: movement.stock_item.variant.product,
-                  location: movement.stock_item.stock_location
-                }
-              end
+          movements.each do |movement|
+            total += movement.quantity
+            if total == 0 && movement.stock_item
+              @inventorys << {
+                date: movement.created_at,
+                product: movement.stock_item.variant.product,
+                location: movement.stock_item.stock_location
+              }
             end
           end
         end
@@ -41,13 +41,20 @@ module ProductsOutStock
       private
       def fix_filter_params
         params[:q] ||= {}
-        params[:q][:created_at_gt] = 
-          params[:q][:created_at_gt].present? ?
-          Time.zone.parse(params[:q][:created_at_gt]).beginning_of_day : Time.zone.now.beginning_of_day
+        params[:q][:created_at_gteq] =
+          params[:q][:created_at_gteq].present? ?
+          Time.zone.parse(params[:q][:created_at_gteq]).beginning_of_day : Time.zone.now.beginning_of_day
 
         params[:q][:created_at_lt] =
           params[:q][:created_at_lt].present? ?
           Time.zone.parse(params[:q][:created_at_lt]).end_of_day : ''
+      end
+
+      def deleted_filter_query
+        query = []
+        query << 'deleted_at >= :deleted_at_gteq' if params[:q][:created_at_gteq].present?
+        query << 'deleted_at <= :deleted_at_lt' if params[:q][:created_at_lt].present?
+        query.join(' AND ')
       end
 
     end
